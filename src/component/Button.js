8dac5d01917a7a35bbeff0ccc9f22b90fc149369
rachanareddychandupatla <chandupatlarachana25@gmail.com>
2021-11-07 01:25:15 +0530

    Show the selected button in blue for 250ms

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./Button.css";
 
-export default class Button extends React.Component {
-  static propTypes = {
-    name: PropTypes.string,
-    orange: PropTypes.bool,
-    wide: PropTypes.bool,
-    clickHandler: PropTypes.func,
+export default function Button(props) {
+  const [currentButton, setCurrentButton] = useState(null);
+  const [show, setShow] = useState(false);
+  useEffect(
+    () => {
+      let timer1 = setTimeout(() => setShow(true), 250);
+      return () => {
+        clearTimeout(timer1);
+      };
+    },
+    [props]
+  );
+
+  const handleClick = () => {
+    props.clickHandler(props.name);
+    setCurrentButton(props.name)
+    setShow(false)
   };
 
-  handleClick = () => {
-    this.props.clickHandler(this.props.name);
-  };
+  const className = [
+    "component-button",
+    props && props.orange ? "orange" : "",
+    props && props.wide ? "wide" : "",
+    props && props.name === currentButton && !show ? "current" : "",
+  ];
 
-  render() {
-    const className = [
-      "component-button",
-      this.props.orange ? "orange" : "",
-      this.props.wide ? "wide" : "",
-    ];
-
-    return (
-      <div className={className.join(" ").trim()}>
-        <button onClick={this.handleClick}>{this.props.name}</button>
-      </div>
-    );
-  }
+  return (
+    <div className={className.join(" ").trim()}>
+      <button onClick={handleClick}>{props.name}</button>
+    </div>
+  )
 }
+
+Button.propTypes = {
+  name: PropTypes.string,
+  orange: PropTypes.bool,
+  wide: PropTypes.bool,
+  clickHandler: PropTypes.func,
+};
